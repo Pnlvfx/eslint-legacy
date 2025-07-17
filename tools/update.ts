82045455822925f19397f8a37229f3ebebb3f20b
simone <simonegauli@gmail.com>
2025-07-17 02:55:48 +0200

    remove updater fuck

--- tools/update.ts
+++
@@ -1,8 +0,0 @@
-import { updateLocalDeps } from '@goatjs/updater';
-
-await updateLocalDeps({
-  '@goatjs/rimraf': 'github:Pnlvfx/goatjs#workspace=@goatjs/rimraf',
-  '@goatjs/ts-unused-exports': 'github:Pnlvfx/goatjs#workspace=@goatjs/ts-unused-exports',
-  '@goatjs/typescript-config': 'github:Pnlvfx/typescript-config',
-  '@goatjs/updater': 'github:Pnlvfx/goatjs#workspace=@goatjs/updater',
-});
